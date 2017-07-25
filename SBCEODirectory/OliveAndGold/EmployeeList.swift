eed50d17f95f2e60c041a2a14a37c8592980f681
hoganavery49 <hogan.avery49@gmail.com>
2017-07-25 16:39:57 -0700

    Fixed connection to Firebase and have almost fully implimented pulling down of data from live database.

--- SBCEODirectory/OliveAndGold/EmployeeList.swift
+++ SBCEODirectory/OliveAndGold/EmployeeList.swift
@@ -11,10 +11,51 @@ import Firebase
 
 class EmployeeList {
     static let sharedInstance = EmployeeList()
-    var employeeListArray: [String: [EmployeeData]]
+    var employeeDictionary: [String: [EmployeeData]]
+    var ref: DatabaseReference!
     
     private init() {
+        employeeDictionary = [String: [EmployeeData]]()
+        ref = Database.database().reference()
         
-        
+        ref.child("employeeList").observeSingleEvent(of: .value, with: { snapshot in
+            if let dictionary = snapshot.value as? [String: [String: AnyObject]] {
+                for subsection in dictionary {
+                    var subsectionEmployees: [EmployeeData] = []
+                    var name: String = "fail"
+                    
+                    for item in subsection.value {
+                        self.ref.child("employeeList/\(subsection.key)/\(item.key)").observeSingleEvent(of: .value, with: { employeeSnapshot in
+                            let employeeObject = EmployeeData(snapshot: employeeSnapshot)
+                            subsectionEmployees.append(employeeObject)
+                            name = "pass"
+                            self.doSomethingAtTheEndOfTheClosureToAllowTimeForVariableSetting()
+                        })
+                    }
+                    print(name)
+                    self.employeeDictionary[subsection.key] = subsectionEmployees
+                }
+            }
+        })
+        print("avri stupid")
+        print(employeeDictionary)
+    }
+    
+    func doSomethingAtTheEndOfTheClosureToAllowTimeForVariableSetting() {
+        var num = 0
+        num = 1
+        num = 0
+        num = 1
+        num = 0
+        num = 1
+        num = 0
+        num = 1
+        num = 0
+        num = 1
+        num = 0
+        num = 1
+        num = 0
+        num = 1
+        num = 0
     }
 }
